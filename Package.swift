c1ef924444f7ed184cf8c60ed5f0cae7a2e37f8e
Riley Testut <riley@rileytestut.com>
2021-11-11 13:12:37 -0800

    Revert "Changes SwiftPM product type to dynamic library"
    This reverts commit baecfc113a229a0b933fff54137a8897fd3982c6.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GPGXDeltaCore",
-            type: .dynamic,
             targets: ["GPGXDeltaCore"]),
     ],
     dependencies: [
