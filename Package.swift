e3a957f4e841694f90cd6f64f421130dfa6e9727
Riley Testut <riley@rileytestut.com>
2023-02-28 14:05:15 -0600

    Raises deployment target to iOS 14.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "GPGXDeltaCore",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/rileytestut/DeltaCore.git", .branch("main"))
+        .package(url: "https://github.com/rileytestut/DeltaCore.git", .branch("ios14"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
